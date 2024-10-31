114293db5f6a75c41d1d53a840a9c367946c7e65
Danny <decheverri123@gmail.com>
2024-10-31 17:57:59 -0400

    header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -7,7 +7,10 @@ export default function Header() {
     <header className="bg-gray-800 px-8 py-4 text-gray-200 shadow-md">
       <nav className="mx-auto flex max-w-7xl items-center justify-between">
         <h1 className="text-2xl font-semibold text-gray-400">
-          Danny Echeverri
+          <Link href="/" className="transition hover:text-yellow-400">
+            {" "}
+            Danny Echeverri{" "}
+          </Link>
         </h1>
         <div className="space-x-8">
           <Link href="/" className="transition hover:text-yellow-400">
