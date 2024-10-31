672db2f9529177abd46244aa545cb1bd60b0983c
Danny <decheverri123@gmail.com>
2024-10-31 12:59:49 -0400

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,18 +132,48 @@ export default function Home() {
         {/* Apps Section */}
         <section className="bg-gray-800 p-6 rounded-lg shadow-md w-full max-w-4xl mt-6">
           <h2 className="text-2xl font-semibold mb-4 text-center">Apps</h2>
-          <div className="grid grid-cols-5 gap-4">
-            {[1, 2, 3, 4, 5].map((num) => (
-              <div key={num} className="relative w-full h-96">
-                <Image
-                  src={`/assets/apps/sparking/0${num}.png`}
-                  alt={`App screenshot ${num}`}
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-md"
-                />
-              </div>
-            ))}
+
+          {/* Sparking Teams Subsection */}
+          <div className="mb-8">
+            <a
+              href="https://github.com/decheverri123/Sparking-Teams/releases"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl font-semibold mb-4 text-center block hover:text-blue-400 text-blue-300"
+            >
+              Sparking Teams
+            </a>
+            <div className="grid grid-cols-5">
+              {[1, 2, 3, 4, 5].map((num) => (
+                <div key={num} className="relative w-full h-96">
+                  <Image
+                    src={`/assets/apps/sparking/0${num}.png`}
+                    alt={`Sparking Teams screenshot ${num}`}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Meditatio Subsection */}
+          <div>
+            <h3 className="text-xl font-semibold mb-4 text-center">
+              Meditatio
+            </h3>
+            <div className="grid grid-cols-5">
+              {[1, 2, 3, 4, 5].map((num) => (
+                <div key={num} className="relative w-full h-96">
+                  <Image
+                    src={`/assets/apps/meditatio/0${num}.png`}
+                    alt={`Meditatio screenshot ${num}`}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </section>
       </main>
