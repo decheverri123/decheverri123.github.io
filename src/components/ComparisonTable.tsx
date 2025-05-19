242432e82fb1dfebc1d95b619cfde57ec81fdb92
Danny <decheverri123@gmail.com>
2025-05-19 14:43:37 -0400

    style: add text-base-content class to ensure proper text color contrast in comparison components

--- src/components/ComparisonTable.tsx
+++ src/components/ComparisonTable.tsx
@@ -15,10 +15,10 @@ const ComparisonTable: React.FC<ComparisonTableProps> = ({
   specifications,
 }) => {
   return (
-    <div className="overflow-x-auto rounded-box border border-base-content/5 mb-8 shadow-xl">
+    <div className="overflow-x-auto rounded-box border border-base-content/5 mb-8 shadow-xl text-base-content">
       <table className="w-full border-collapse xs:text-sm">
         <thead>
-          <tr className="xs:text-xs sm:text-sm md:text-md font-display">
+          <tr className="xs:text-xs sm:text-sm md:text-md font-display text-base-content">
             <th className="text-left py-3 px-4 bg-base-200 font-bold uppercase ">
               Feature
             </th>
@@ -37,14 +37,20 @@ const ComparisonTable: React.FC<ComparisonTableProps> = ({
           {specifications.map((row, rowIndex) => (
             <tr
               key={rowIndex}
-              className="border-b border-base-200 xs:text-xs sm:text-base font-serif"
+              className="border-b border-base-200 xs:text-xs sm:text-base font-serif text-base-content"
             >
               <td className="py-3 px-4 xs:pr-1 font-semibold text-base-content bg-base-200 font-display">
                 {row.feature}
               </td>
-              <td className="py-3 px-4 bg-base-100">{row.prima}</td>
-              <td className="py-3 px-4 bg-base-100">{row.secunda}</td>
-              <td className="py-3 px-4 bg-base-100">{row.terza}</td>
+              <td className="py-3 px-4 bg-base-100 text-base-content">
+                {row.prima}
+              </td>
+              <td className="py-3 px-4 bg-base-100 text-base-content">
+                {row.secunda}
+              </td>
+              <td className="py-3 px-4 bg-base-100 text-base-content">
+                {row.terza}
+              </td>
             </tr>
           ))}
         </tbody>
